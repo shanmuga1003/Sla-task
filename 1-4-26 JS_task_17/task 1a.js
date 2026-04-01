516674b406250491e3ef0c8c0b3313a34015e2b7
shanmuga1003 <shanmugap089@gmail.com>
2026-04-01 21:24:15 +0530

    Add files via upload

---
+++ 1-4-26 JS_task_17/task 1a.js
@@ -0,0 +1,12 @@
+// Task 1:
+// a. Check if a number is positive or negative.
+
+let num = -5;
+
+if (num > 0) {
+    console.log("Positive number");
+} else if (num < 0) {
+    console.log("Negative number");
+} else {
+    console.log("Zero");
+}
